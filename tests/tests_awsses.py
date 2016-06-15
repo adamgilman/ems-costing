91fa53001f86326253f1a5a4845673aa4b405b13
Adam Gilman <adam.gilman@gmail.com>
2016-06-15 14:33:35 +0100

    AWS SES Completed

--- tests/tests_awsses.py
+++ tests/tests_awsses.py
@@ -3,7 +3,6 @@ from vendors import AWSSES
 import unittest
 
 class TestAWSSES(unittest.TestCase):
-
     def setUp(self):
         self.vendor = AWSSES()
 
@@ -17,4 +16,10 @@ class TestAWSSES(unittest.TestCase):
 
     #amazon calculates costs per day instead of per month
     def test_100k_pm(self):
-        self.assertEqual(self.vendor.getPrice(100000), 3.74)
+        self.assertEqual(self.vendor.getPrice(100000), 4)
+
+    def test_150k_pm(self):
+        self.assertEqual(self.vendor.getPrice(150000), 9)
+
+    def test_200k_pm(self):
+        self.assertEqual(self.vendor.getPrice(200000), 14)
--- vendors/awsses.py
+++ vendors/awsses.py
@@ -1,3 +1,9 @@
 class AWSSES(object):
     def getPrice(self, numberofemails):
-        return 0
+        #first 62000 free
+        if numberofemails <= 62000:
+            return 0
+        originalnumber = numberofemails
+        numberofemails = numberofemails - 62000
+        #Email messages are charged at $0.10 per 1,000.
+        return round(.1 * numberofemails/1000, 0)
